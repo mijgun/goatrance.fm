e1081285c1d16e2d907738ceffabd60b6705b454
mijgun <m@isayev.me>
2024-06-01 18:45:14 +0300

    visuals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import AudioPlayer from './components/AudioPlayer';
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audio = new Audio('https://goa4ever.ilovemijgun.com:8443/stream');
+
+  const togglePlayPause = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Radio App</h1>
+        <h1>Goa4Ever Radio</h1>
+        <p>Now Playing:</p>
+        <button onClick={togglePlayPause}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
       </header>
-      <AudioPlayer />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// Регистрируем serviceWorker
-serviceWorker.register();
+reportWebVitals();
