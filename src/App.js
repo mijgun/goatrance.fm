7074caeea84e904856a9dfa2ad1cf5d15ae735ca
mijgun <m@isayev.me>
2024-06-01 18:53:17 +0300

    Added audio type attribute for stream

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Goa4Ever Radio</h1>
-        <audio ref={audioRef} controls />
+        <audio ref={audioRef} controls>
+          <source src="https://goa4ever.ilovemijgun.com:8443/stream" type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
         <div>
           <button onClick={handlePlay}>Play</button>
           <button onClick={handlePause}>Pause</button>
